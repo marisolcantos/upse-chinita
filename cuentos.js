56b64ff3df38f3bd8448b07909aa34de24984734
Marisol Cantos <marisol_c_88@hotmail.com>
2016-03-27 10:56:42 -0500

    se subieron archivos cuentos

---
+++ cuentos.js
@@ -0,0 +1,16 @@
+console.log("inicio de programa");
+var preguntas = 3;
+var preguntasfaltantes ="quedan " + preguntas + "preguntas";
+alert("este es el juego del cuenta cuentos.  sigue las instrucciones .");
+var sustantivo =prompt("ingresa un sustantivo."  + preguntasfaltantes);
+preguntas = preguntas - 1;
+preguntasfaltantes = "quedan " + preguntas + "preguntas";
+var adjetivo1 = prompt("ingrese un adjetivo" + preguntas faltantes);
+preguntas = prguntas -1;
+preguntasfaltantes ="quedan"+preguntas +"preguntas";
+var verbo1 = prompt("ingrese un verbo" + preguntas faltantes);
+
+
+var cuento = "el" + adjetivo1 + " " + sustantivo1 + " queria" + verbo1 + ""
+document.write("<h1>cuento</h1><p>E1 
+	console.log (" programa completo");
---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+
+console.log("inicio del programa");
+var dado = Math.floor(match.random()  *6)  +  1;
+alert("Dado dice:")  + dado);
+console.log("programa completado");
+
+
+
+
+
+
+
+
+
+console.log ("inicio de programa");
+var peliculasvistas= prompt("cuantas peliculas has visto en el mes")
+var seriesvistas= prompt ("cuantas series has visto en el mes");
+var tiempopantalla= parsetInt("peliculasvistas") + tiempopantalla ("cuantas series has visto en el mes");
+
