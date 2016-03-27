878ddf5f36b6db324c29ac028380466319e73441
Marisol Cantos <marisol_c_88@hotmail.com>
2016-03-27 12:12:08 -0500

    ingresamos mayor y menor adivinando

--- scripts.js
+++ scripts.js
@@ -1,20 +1,51 @@
 
+console.log("inicio programa");
+var numeroUsuario =prompt("ingrese un numero del 1 al 5");
+var numeroAdivinar = 3;
+if (parseInt(numeroUsuario)===numeroAdivinar)
+{
+	adivinoCorrectamente = true;
 
-console.log("inicio del programa");
-var dado = Math.floor(match.random()  *6)  +  1;
-alert("Dado dice:")  + dado);
-console.log("programa completado");
+}
+else if  (parseInt(numeroUsuario) > numeroAdivinar)
+
+{
+	alert("el numero que ingresaste es mayor");
+	var nuevointento = prompt("intento de nuevo,ingrese un numero del 1 al 5");
+	if (parseInt(nuevointento)===numeroAdivinar)
+	{
+	adivinoCorrectamente = true;
+}
+ else
+ {
+    adivinoCorrectamente = false;
+}
+}
+else if ((parseInt(numeroUsuario) < numeroAdivinar))
+{
+   alert("el numero que ingresaste es menor");
+	var nuevointento = prompt("intento de nuevo,ingrese un numero del 1 al 5");
+	if (parseInt(nuevointento)===numeroAdivinar)
+	{
+	adivinoCorrectamente = true;
+}  
+else
+{
+adivinoCorrectamente = false;
+}
+}
+if (adivinoCorrectamente)
+{
+	document.write("<p>ADIVINASTE. BUEN TRABAJO.!!!</P>");
+
+}
+else
+{
+	document.write("<p>NO ADIVINASTE. INTENTALO DE NUEVO =</P>");
+}
+console.log("PROGRAMA COMPLETO");
 
 
 
 
-
-
-
-
-
-console.log ("inicio de programa");
-var peliculasvistas= prompt("cuantas peliculas has visto en el mes")
-var seriesvistas= prompt ("cuantas series has visto en el mes");
-var tiempopantalla= parsetInt("peliculasvistas") + tiempopantalla ("cuantas series has visto en el mes");
 
